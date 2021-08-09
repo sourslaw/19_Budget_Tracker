5350ce08830b486b57b5d8837f0ec37369a25838
Scott Lawan <scottlawan@Scotts-Mac-mini.local>
2021-08-09 10:31:44 -0600

    updating db.js

--- public/db.js
+++ public/db.js
@@ -1,61 +1,59 @@
 let db;
 let budgetVersion;
-
-// Creating a new db request for a "budget" database.
-const request = indexedDB.open('BudgetDB', budgetVersion || 31);
-
+// creating a request for an indexedDB database
+const request = indexedDB.open('BudgetDB', budgetVersion || 3);
+// changing the db version 
 request.onupgradeneeded = function (e) {
-  const { oldVersion } = e;
-  const newVersion = e.newVersion || db.version;
   db = e.target.result;
+  // if db is empty, create new
   if (db.objectStoreNames.length === 0) {
-    db.createObjectStore('BudgetStore', { autoIncrement: true });
+    db.createObjectStore('BudgetStorage', { autoIncrement: true });
   }
 };
-
-request.onerror = function (e) {
-  console.log(`error ${e.target.errorCode} . . .`);
-};
-
+// reading and writing
 function checkDatabase() {
-  let transaction = db.transaction(['BudgetStore'], 'readwrite');
-  const store = transaction.objectStore('BudgetStore');
-  const getAll = store.getAll();
-
-  getAll.onsuccess = function () {
-    if (getAll.result.length > 0) {
+  // new transaction
+  let transaction = db.transaction(['BudgetStorage'], 'readwrite');
+  // accesssing the store object
+  const fromStore = transaction.objectStore('BudgetStorage');
+  // getting all stored objects and setting to getAll
+  const allObjs = fromStore.getAll();
+  // if successfull . . .
+  allObjs.onsuccess = function () {
+    if (allObjs.result.length > 0) {
       fetch('/api/transaction/bulk', {
         method: 'POST',
-        body: JSON.stringify(getAll.result),
+        body: JSON.stringify(allObjs.result),
         headers: {
           Accept: 'application/json, text/plain, */*',
           'Content-Type': 'application/json',
         },
       })
         .then((response) => response.json())
+        // if response is not empty
         .then((res) => {
           if (res.length !== 0) {
-            transaction = db.transaction(['BudgetStore'], 'readwrite');
-            const currentStore = transaction.objectStore('BudgetStore');
-            currentStore.clear();
-            console.log('Clearing store 🧹');
+            transaction = db.transaction(['BudgetStorage'], 'readwrite');
+            // current store is assigne to variable recentStore
+            const recentStore = transaction.objectStore('BudgetStorage');
+            // clear store
+            recentStore.clear();
           }
         });
     }
   };
-}
-
+// checks if application is connected to wifis
 request.onsuccess = function (e) {
   db = e.target.result;
   if (navigator.onLine) {
     checkDatabase();
   }
 };
-
+// pushes new data to online storage
 const saveRecord = (record) => {
-  const transaction = db.transaction(['BudgetStore'], 'readwrite');
-  const store = transaction.objectStore('BudgetStore');
-  store.add(record);
+  const transaction2 = db.transaction(['BudgetStorage'], 'readwrite');
+  const fromStore2 = transaction2.objectStore('BudgetStorage');
+  fromStore2.add(record);
 };
-
+// checks if app is online or not
 window.addEventListener('online', checkDatabase);
